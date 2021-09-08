c151865b32827305a03356ea28cb79d894a49740
tcreusc <toncc3d@gmail.com>
2021-09-08 16:49:09 +0200

    elimino classes obsoletes

--- code/computeInternalDisplacements.m
+++
@@ -1,7 +0,0 @@
-function  ue = computeInternalDisplacements(dim, e, Td, u)
-    for i = 1:dim.nne*dim.ni
-            I = Td(e,i);
-            ue(i,1) = u(I);
-    end
-end
-
--- code/test_func.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = test_func(inputArg1,inputArg2)
-%TEST_FUNC Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
